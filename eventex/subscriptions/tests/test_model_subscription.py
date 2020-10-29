25265d2d736d5453e35c64e7a8a6c6bb97dadefc
victorhugo <victorblog410@gmail.com>
2020-10-28 22:40:23 -0300

    Persiste a inscricao

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription (
+            name='Victor Hugo',
+            cpf='12345678901',
+            email='victor-hugo-py@outlook.com',
+            phone='81-998832982'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())  # Verificar se existe no banco de dados
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
